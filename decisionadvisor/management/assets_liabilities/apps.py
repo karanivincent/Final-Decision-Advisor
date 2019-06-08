9acc7f1ff262def9e936e1ae7e15f581b6a51cfa
karanivincent <trevorvinnie@gmail.com>
2019-06-08 12:43:09 +0300

    'creating applications'

---
+++ decisionadvisor/management/assets_liabilities/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AssetsLiabilitiesConfig(AppConfig):
+    name = 'assets_liabilities'
---
+++ decisionadvisor/management/humanresource/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HumanresourceConfig(AppConfig):
+    name = 'humanresource'
